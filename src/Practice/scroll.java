67636a30ac9039cca340c0fb608d9afe0d87afd4
2091793 <2091793@LTIN439050.cts.com>
2024-04-09 14:30:08 +0530

    new

---
+++ src/Practice/scroll.java
@@ -0,0 +1,22 @@
+package Practice;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class scroll {
+
+	public static void main(String[] args) {
+
+		//Initiate Chrome driver
+		ChromeDriver driver =new ChromeDriver();
+		driver.manage().window().maximize();
+		//Open Url
+		driver.get("https://the-internet.herokuapp.com/");
+		//Javascript scroll 
+		JavascriptExecutor js= (JavascriptExecutor)driver;
+		 //js.executeScript("scrollBy(0, 4500)");
+		js.executeScript("scrollBy(0,300)");
+		js.executeScript("scrollBy(0,700)");
+		
+	}
+	
+
+}
