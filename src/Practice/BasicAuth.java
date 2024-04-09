75981d589957d68cc9451045ec12d182e14dc0e3
2091793 <2091793@LTIN439050.cts.com>
2024-04-09 16:02:18 +0530

    new

---
+++ src/Practice/BasicAuth.java
@@ -0,0 +1,23 @@
+package Practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicAuth {
+
+	public static void main(String[] args) {
+		// Initiate Chrome driver
+		ChromeDriver driver =new ChromeDriver();
+		driver.manage().window().maximize();
+		//open url
+		String username= "admin";
+		String password= "admin";
+		String domain = "the-internet.herokuapp.com/basic_auth";
+		String URL ="https://"+username+":"+password+"@"+domain;
+		driver.get(URL);
+		System.out.println(driver.findElement(By.xpath("//p[contains(text(),'Congratulations! You must have the proper credentials')]")).getText());
+		driver.quit();
+		
+	}
+
+}
