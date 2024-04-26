2d49011b5f36332225d1b2993a2172affec32e41
2091793 <2091793@LTIN439050.cts.com>
2024-04-26 12:59:29 +0530

    new

--- src/programs/Try1.java
+++ src/programs/Try1.java
@@ -1,6 +1,8 @@
 package programs;
 import java.time.Duration;
 import java.util.List;
+import java.util.Scanner;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,6 +14,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Try1 {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) throws InterruptedException {
 		//chrome
 		WebDriverManager.chromedriver().setup();
@@ -23,17 +26,18 @@ public class Try1 {
 		WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(30));
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//a[normalize-space()='Sony vaio i7'])[1]")));
 		List<WebElement> productList = driver.findElements(By.xpath("//body//h4"));
-		String productName ="Nokia lumia 1520";
-		for (int i = 0; i < productList.size(); i++) {
-
-
-			if (productList.get(i).getText().equals(productName)) 
-			{
-				productList.get(i).click();	
-				break;
-			}
-
+		// Loop for getting products texts
+		for (int j = 0; j < productList.size(); j++) {
+			String listmobile =productList.get(j).getText();
+			System.out.println(j +"." + listmobile );		
 		}
+		// Print products list and get desired products 
+		System.out.println("Enter Product Index which you want to buy");
+		Scanner number = new Scanner(System.in);		
+		int i =number.nextInt();
+		//click on desired product 
+		productList.get(i).click();	
+
 
 
 	}
