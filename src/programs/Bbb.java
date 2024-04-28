d24987d735b1d091d4356bacd9f6e137457644aa
Arjun <2091793@LTIN439050.cts.com>
2024-04-28 22:31:56 +0530

    First Commit

--- src/programs/Bbb.java
+++ src/programs/Bbb.java
@@ -1,9 +1,22 @@
 package programs;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Bbb {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Set<String> kiSet = new HashSet<String>();
+		kiSet.add("xpu");
+		kiSet.add("Arjun");
+		kiSet.add("Zee");
+		kiSet.add("Arun");
+		kiSet.add("Amazon");
+		for (String string : kiSet) {
+			System.out.println(string);
+			
+		}
 
 	}
 
