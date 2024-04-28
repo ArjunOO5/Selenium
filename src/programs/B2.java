1700d894c971ef559bf50673c246d5c25014db4d
2091793 <2091793@LTIN439050.cts.com>
2024-04-28 20:03:49 +0530

    new

---
+++ src/programs/B2.java
@@ -0,0 +1,14 @@
+package programs;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class B2 {
+
+	public static void main(String[] args) {
+		
+		ChromeDriver driver =new ChromeDriver();
+		driver.manage().window().maximize();
+
+	}
+
+}
